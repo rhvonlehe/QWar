061f7d321b698e85282fccd1fc7ba1bbf45dd319
Rich von Lehe <rhvonlehe@gmail.com>
2023-04-18 22:21:03 -0500

    conanfile.py added and working, but now for the conan-provided-deps case need to have a custom command the brings Qt deps into build folder

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
+
+class QWar(ConanFile):
+    name = "QWar"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "boost/1.77.0", "qt/6.4.2"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+
+    def configure(self):
+        # requiring qt doesn't get all modules, some have to be explicitly added as
+        # shown here
+        self.options["qt"].qtdeclarative = True
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
